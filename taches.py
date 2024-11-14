d8405f9c1a0f4ca27b5f8ec8bf285ece147e3301
MelodyDuplaix <melo.surseine@gmail.com>
2024-11-14 17:04:56 +0100

    Ajout d'un bouton pour annuler la dernière action en cas de missclick

--- taches.py
+++ taches.py
@@ -5,24 +5,24 @@ import json
 import os
 from datetime import datetime
 
-# Fonction pour charger les données depuis le fichier JSON
+
 def load_data(filename):
     if os.path.exists(filename):
         with open(filename, 'r', encoding='utf-8') as f:
             return json.load(f)
     return {}
 
-# Fonction pour sauvegarder les données dans le fichier JSON
+
 def save_data(filename, data):
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
-# Fonction pour enregistrer l'action dans un fichier CSV
+
 def log_action(filename, manager, action_type, name, value):
     with open(filename, 'a', encoding='utf-8') as f:
         f.write(f"{datetime.now().strftime('%d/%m/%y %H:%M:%S')},{manager},{action_type},{name},{value}\n")
 
-# Formatage de la page avec CSS
+
 def formatage_de_la_page(fichier_css):
     with open(fichier_css) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -42,11 +42,15 @@ formatage_de_la_page("style.css")
 
 sous_manager_selection = st.sidebar.selectbox("Sélectionnez un sous-manager", list(data.get("managers", {}).keys()))
 
-# Charger les données spécifiques au sous-manager sélectionné
+
 sous_manager_data = data["managers"].get(sous_manager_selection, {})
 st.session_state.objectif = sous_manager_data.get("objectif", 10)
 st.session_state.daily = sous_manager_data.get("daily", 0)
 st.session_state.total = sous_manager_data.get("total", 0)
+
+
+if "last_action" not in st.session_state:
+    st.session_state.last_action = None
 
 col_blank, col_general = st.columns([1, 10])
 with col_general:
@@ -72,25 +76,63 @@ with col_general:
         sous_manager_data['daily'] = 0
         save_data(data_file, data)
 
-    st.button("Commencer la journée", key="start_day", on_click=reset_daily, type="secondary")
-
     def retirer_recompense(valeur, recompense):
+        
+        st.session_state.last_action = {
+            "type": "recompense",
+            "valeur": valeur,
+            "nom": recompense
+        }
         st.session_state.total -= valeur
         sous_manager_data['total'] = st.session_state.total
         save_data(data_file, data)
         log_action(log_file, sous_manager_selection, "Récompense", recompense, valeur)
 
     def ajouter_tache(valeur, nom_tache, tache_ponctuelle=False):
+        
+        st.session_state.last_action = {
+            "type": "tache",
+            "valeur": valeur,
+            "nom": nom_tache,
+            "tache_ponctuelle": tache_ponctuelle
+        }
         st.session_state.total += valeur
         st.session_state.daily += valeur
         sous_manager_data['daily'] = st.session_state.daily
         sous_manager_data['total'] = st.session_state.total
         save_data(data_file, data)
         log_action(log_file, sous_manager_selection, "Tâche", nom_tache, valeur)
-        # Supprimer la tâche ponctuelle après l'exécution
+        
+        
         if tache_ponctuelle:
             del sous_manager_data['taches_ponctuelles'][nom_tache]
             save_data(data_file, data)
+
+    
+    def annuler_derniere_action():
+        if st.session_state.last_action:
+            last_action = st.session_state.last_action
+            if last_action["type"] == "recompense":
+                st.session_state.total += last_action["valeur"]
+            elif last_action["type"] == "tache":
+                st.session_state.total -= last_action["valeur"]
+                st.session_state.daily -= last_action["valeur"]
+                if last_action["tache_ponctuelle"]:
+                    sous_manager_data['taches_ponctuelles'][last_action["nom"]] = last_action["valeur"]
+            
+            sous_manager_data['total'] = st.session_state.total
+            sous_manager_data['daily'] = st.session_state.daily
+            save_data(data_file, data)
+            
+            st.session_state.last_action = None
+            st.rerun()
+
+    col_commencer, col_annuler = st.columns([3, 2])
+    with col_commencer:
+        st.button("Commencer la journée", key="start_day", on_click=reset_daily, type="secondary")
+    with col_annuler:
+        if st.button("Annuler la dernière action", type= "primary"):
+            annuler_derniere_action()
 
     st.header("Récompenses")
     for recompense, valeur in sous_manager_data.get('recompenses', {}).items():
@@ -98,7 +140,7 @@ with col_general:
 
     st.header("Tâches")
 
-    # Gestion des tâches récurrentes
+    
     sous_types = {}
     for tache, (valeur, sous_type) in sous_manager_data.get('taches', {}).items():
         if sous_type not in sous_types:
@@ -112,14 +154,14 @@ with col_general:
             for tache, valeur in taches:
                 st.button(f"{tache} (+{valeur})", on_click=ajouter_tache, args=(valeur, tache))
 
-    # Gestion des tâches ponctuelles
+    
     st.header("Tâches Ponctuelles")
 
-    # Afficher les tâches ponctuelles existantes
+    
     for tache, valeur in sous_manager_data.get('taches_ponctuelles', {}).items():
         st.button(f"{tache} (+{valeur})", on_click=ajouter_tache, args=(valeur, tache, True))
 
-    # Formulaire pour ajouter une nouvelle tâche ponctuelle
+    
     with st.expander("Ajouter une tâche ponctuelle"):
         nom_tache_ponctuelle = st.text_input("Nom de la tâche ponctuelle")
         valeur_tache_ponctuelle = st.number_input("Valeur de la tâche ponctuelle", min_value=0, step=1)
@@ -127,3 +169,4 @@ with col_general:
             sous_manager_data.setdefault('taches_ponctuelles', {})[nom_tache_ponctuelle] = valeur_tache_ponctuelle
             save_data(data_file, data)
             st.rerun()
+
